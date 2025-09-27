463e77e690596e65520878ecc8a8d846637ae63c
zhiwang24 <zhiwang224@gmail.com>
2025-09-27 04:15:28 -0400

    skill icon update

--- src/components/SkillsData.js
+++ src/components/SkillsData.js
@@ -2,38 +2,36 @@ export const SkillsData = [
     { 
       label: "Languages", 
       items: [
-        { name: "Java", logo: "https://cdn.jsdelivr.net/npm/@programming-languages-logos/java@0.0.0/+esm"},
-        { name: "Python", logo: "/logos/python.svg" },
-        { name: "C", logo: "/logos/c.svg" },
-        { name: "JavaScript", logo: "/logos/javascript.svg" },
-        { name: "TypeScript", logo: "/logos/typescript.svg" },
-        { name: "SQL", logo: "/logos/sql.svg" },
+        { name: "Java", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"},
+        { name: "Python", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" },
+        { name: "C", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" },
+        { name: "JavaScript", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" },
+        { name: "TypeScript", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" },
+        { name: "SQL", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqldeveloper/sqldeveloper-original.svg" },
       ] 
     },
     { 
       label: "Frameworks", 
       items: [
         { name: "React", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-        { name: "JUnit", logo: "/logos/junit.svg" },
-        { name: "Jest", logo: "/logos/jest.svg" },
-        { name: "Pandas", logo: "/logos/pandas.svg" },
-        { name: "Matplotlib", logo: "/logos/matplotlib.svg" },
-        { name: "Selenium", logo: "/logos/selenium.svg" },
-        { name: "Playwright", logo: "/logos/playwright.svg" },
-      ] 
+        { name: "JUnit", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/junit/junit-original.svg" },
+        { name: "Jest", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg" },
+        { name: "Pandas", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original.svg" },
+        { name: "Matplotlib", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/matplotlib/matplotlib-original.svg" },
+        { name: "Selenium", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/selenium/selenium-original.svg" },
+        ]
     },
     { 
       label: "Tools", 
       items: [
-        { name: "Git", logo: "/logos/git.svg" },
-        { name: "Postman", logo: "/logos/postman.svg" },
-        { name: "AWS EC2", logo: "/logos/aws-ec2.svg" },
-        { name: "MySQL", logo: "/logos/mysql.svg" },
-        { name: "PostgreSQL", logo: "/logos/postgresql.svg" },
-        { name: "Tableau", logo: "/logos/tableau.svg" },
-        { name: "Agile", logo: "/logos/agile.svg" },
-        { name: "Figma", logo: "/logos/figma.svg" },
-        { name: "Docker", logo: "/logos/docker.svg" },
+        { name: "Git", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" },
+        { name: "Postman", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" },
+        { name: "AWS EC2", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" },
+        { name: "MySQL", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg" },
+        { name: "PostgreSQL", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" },
+        { name: "Android Studio", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/androidstudio/androidstudio-original.svg" },
+        { name: "Figma", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" },
+        { name: "Docker", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg" },
       ] 
     },
   ];
